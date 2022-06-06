1724a175a87fddce706642fda25c18c7b16e2763
AnandAVT <blackyvenky21@gmail.com>
2022-06-06 08:00:19 +0530

    use async await functionailty in some places to handle mongoDB

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,37 +3,34 @@
 const { populate } = require('../models/post');
 const Post = require('../models/post');
 const User = require('../models/user');
-module.exports.home = function(req,res) {
+module.exports.home = async function(req,res) {
 
-    var allUsers;
-    
-    if(req.user)
-    {
-        User.find({},(err,users) => {
-           allUsers = users;     
-        })
+    try{
+        var allUsers;
+        
+        if(req.user)
+        {
+            await User.find({},(err,users) => {
+            allUsers = users;     
+            })
+        }
+        
+        let posts = await Post.
+            find({}).
+            populate("user").
+            populate({
+                path:"comments",
+                populate:{
+                    path:"user"
+                }
+            });
+
+        return res.render('home',{
+            posts:posts,
+            all_users:allUsers
+        });
     }
-    
-    Post.
-    find({}).
-    populate("user").
-    populate({
-        path:"comments",
-        populate:{
-            path:"user"
-        }
-    }). 
-    exec((err,posts) => {
-        if(err)
-        {
-           return res.render('<h1> Not able to fetch the post </h1>');
-        }
-        else
-        {
-            return res.render('home',{
-                        posts:posts,
-                        all_users:allUsers
-                    });
-        }
-    });
+    catch(err){
+         console.log('Error: ',err);
+    }    
 };
